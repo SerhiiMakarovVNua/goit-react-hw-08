524f304151e63f6bfd797a6c7ec48fc67d100556
SerhiiMakarovVNua <serhiimakarov79@gmail.com>
2024-11-16 16:27:00 +0200

    react-hw-08.02

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { AppBar } from "./appBar/AppBar";
+import { AppBar } from "./AppBar/AppBar";
 import Loader from "./Loader/Loader";
 
 export const Layout = ({ children }) => {
